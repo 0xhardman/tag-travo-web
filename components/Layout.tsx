74d9cf7a188827cbc1f548bbe8c45fc5f86be533
0xhardman <1142783160@qq.com>
2023-11-17 23:19:04 +0300

    feat: navigator

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,14 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+    const router = useRouter()
+    const [current, setCurrent] = useState<'swaper' | 'sender'>('swaper')
+    useEffect(() => {
+        router.asPath.includes('swaper') && setCurrent('swaper')
+        router.asPath.includes('sender') && setCurrent('sender')
+    }, [router])
     return <div className=''>
         <div className='flex justify-between items-center w-screen h-[70px] border-b px-[20px]'>
             <div className='flex gap-4 items-center'>
@@ -10,9 +18,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                 </div>
             </div>
             <div className='flex justify-spacing items-center gap-8 text-[#7d7d7d]'>
-                <div>Intro</div>
-                <div>Swaper</div>
-                <div>Sender</div>
+                <div onClick={
+                    () => router.push('/swaper')
+                } className={`cursor-pointer ${current == 'swaper' ? 'text-[#008192]' : ''}`}>Swaper</div>
+                <div onClick={
+                    () => router.push('/sender')
+                } className={`cursor-pointer ${current == 'sender' ? 'text-[#008192]' : ''}`}
+                >Sender</div>
             </div>
             <ConnectButton showBalance={false} chainStatus="icon" />
             {/* <div className='w-[120px]'></div> */}
---
+++ pages/swaper.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image'
+import { Albert_Sans } from 'next/font/google'
+import clsx from 'clsx'
+import Arrow from '@/components/Arrow'
+import { useState } from 'react'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import Pay from '@/components/Pay'
+import Receive from '@/components/Receive'
+import { Tag } from '@/type'
+import Layout from '@/components/Layout'
+import Tip from '@/components/Tip'
+
+const inter = Albert_Sans({ subsets: ['latin'] })
+
+export default function Home() {
+  const [pay, setPay] = useState('0')
+  const [tag, setTag] = useState<Tag>({
+    tag: '',
+    description: 'Frozen yoghurtFrozen yoghurtFrozen yoghurtFrozen yoghurtFrozen yoghurt',
+    count: 1,
+    price: 0.01,
+  })
+  return (
+    <Layout>
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className} bg-[#fffeff]`}
+      >
+        <div className={clsx('flex relative flex-col gap-[6px]',
+          'w-[462px]  px-[8px] py-[12px]',
+          'bg-white border-[#22222243] border rounded-[24px]',
+          'text-[#7c7c7c]')}>
+          <Pay pay={pay} setPay={setPay} />
+          <div className='left-[200px] top-[120px] rounded-xl absolute flex justify-center items-center box-content bg-[#f9f9f9] border-[4px] border-white w-[32px] h-[32px]'>
+            <img src="/down-arrow.svg" alt="" />
+          </div>
+          <Receive tag={tag} setTag={setTag} payUSD={Number(pay)} />
+          <Tip tag={tag} />
+          <div className='w-full rounded-[18px] bg-[#1f7f94] text-[20px] text-white h-[58px] flex justify-center items-center'>
+            Swap
+          </div>
+        </div>
+      </main>
+    </Layout>
+  )
+}
