7c5edf5835ae24d9a5e39e4bdd09a6a5d9cf1248
0xhardman <1142783160@qq.com>
2023-11-17 21:10:12 +0300

    feat: data swap page style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,94 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Albert_Sans } from 'next/font/google'
+import clsx from 'clsx'
+import Arrow from '@/components/Arrow'
+import { useState } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Albert_Sans({ subsets: ['latin'] })
 
 export default function Home() {
+  const [pay, setPay] = useState(0)
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className} bg-[#fffeff]`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className={clsx('flex relative flex-col gap-[6px]',
+        'w-[462px]  px-[8px] py-[12px]',
+        'bg-white border-[#22222243] border rounded-[24px]',
+        'text-[#7c7c7c]')}>
+        <div className={clsx('flex flex-col',
+          'h-[12opx] p-[16px]',
+          'bg-[#f9f9f9] rounded-[16px]',
+          'text-[14px] '
+        )}>
+          <div>You pay</div>
+          <div className='flex justify-between items-center'>
+            <div className='text-[36px]'><input className='w-full bg-[#f9f9f9] outline-none' type="text" value={pay} onChange={(e) => { setPay(e.value) }} /></div>
+            <div className='px-[4px] flex justify-start items-center shadow-md border border-[#22222222] gap-1 w-[107px] h-[34px] bg-white rounded-full'>
+              <div className='w-[24px] h-[24px] bg-[#f7ce46] rounded-full mr-[4px]'></div>
+              <span className='text-[18px] font-semibold text-black'>BNB</span>
+              {/* <div>^</div> */}
+              <Arrow color='black' />
+            </div>
+
+          </div>
+          <div className='flex gap-2 justify-between'>
+            <div className='flex gap-2 justify-end'>
+              $ 123
+            </div>
+            <div className='flex gap-2 justify-end'>
+              <div>Balance: 0.2123</div>
+              <div className='text-[##1f7f94] font-semibold'>Max</div>
+            </div>
+          </div>
+        </div>
+        <div className='left-[200px] top-[120px] rounded-xl absolute flex justify-center items-center box-content bg-[#f9f9f9] border-[4px] border-white w-[32px] h-[32px]'>
+          <img src="/down-arrow.svg" alt="" />
+        </div>
+        <div className={clsx('flex flex-col',
+          'h-[12opx] p-[16px]',
+          'bg-[#f9f9f9] rounded-[16px]',
+          'text-[14px] text-[#7c7c7c]'
+        )}>
+          <div>You Receive</div>
+          <div className='flex justify-between items-center'>
+            <div className='text-[36px]'>0</div>
+            <div className='px-[12px] flex justify-start items-center shadow-md bg-[#1f7f94] text-white gap-1  h-[34px] rounded-full text-[20px]'>
+              Select data
+              <Arrow color="white" />
+            </div>
+
+          </div>
+          <div className='flex gap-2 justify-between'>
+            <div className='flex gap-2 justify-end'>
+              $ 123
+            </div>
+            <div className='flex gap-2 justify-end'>
+              <div>Balance: 0.2123</div>
+            </div>
+          </div>
+        </div>
+        <div className={clsx('flex justify-between',
+          'h-[50px] p-[16px]',
+          'bg-white rounded-[16px] border border-[#22222213]',
+          'text-[14px] text-[#7c7c7c]'
+        )}>
+          <div className='text-black'>
+            1 USDT=0.00048 BNB
+            <span className='text-[#7c7c7c]'>($1.00)</span>
+          </div>
+          <div className='flex items-center'>
+            <img className='w-[18px] mr-[4px] opacity-40 ' src="/gas.svg" alt="" />
+            <div>$0.23</div>
+            <Arrow color="#cecece" />
+            {/* <img className='w-[24px] mr-[4px]' src="/arrow.svg" alt="" /> */}
+          </div>
+        </div>
+        <div className='w-full rounded-[18px] bg-[#1f7f94] text-[20px] text-white h-[58px] flex justify-center items-center'>
+          Swap
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
