6765968871f71ce22555fce669a9097b943782e6
0xhardman <1142783160@qq.com>
2023-11-17 23:15:24 +0300

    cmp: tip

---
+++ components/Tip.tsx
@@ -0,0 +1,17 @@
+import { Tag } from "@/type";
+import clsx from "clsx";
+
+export default function Tip({ tag }: { tag: Tag }) {
+    return <div className={clsx('flex justify-center',
+        'h-[50px] p-[16px]',
+        'bg-white rounded-[16px] border border-[#22222213]',
+        'text-[14px] text-[#7c7c7c]'
+    )}>
+        <div className='text-black'>
+            {
+                tag.tag == '' ? 'Please Select Tag' : <>
+                    1 BNB can send {tag.count} users with <span className='border bg-[#008093] p-[4px] rounded-lg text-white'>{tag?.tag}</span> {(1 / tag.price).toFixed(0)} times mails.</>
+            }
+        </div>
+    </div>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import Pay from '@/components/Pay'
 import Receive from '@/components/Receive'
 import { Tag } from '@/type'
 import Layout from '@/components/Layout'
+import Tip from '@/components/Tip'
 
 const inter = Albert_Sans({ subsets: ['latin'] })
 
@@ -33,20 +34,7 @@ export default function Home() {
             <img src="/down-arrow.svg" alt="" />
           </div>
           <Receive tag={tag} setTag={setTag} payUSD={Number(pay)} />
-          <div className={clsx('flex justify-center',
-            'h-[50px] p-[16px]',
-            'bg-white rounded-[16px] border border-[#22222213]',
-            'text-[14px] text-[#7c7c7c]'
-          )}>
-            <div className='text-black'>
-              1 BNB can send {tag.count} users with <span className='border bg-[#008093] p-[4px] rounded-lg text-white'>{tag?.tag}</span> {(1 / tag.price).toFixed(0)} times mails.
-            </div>
-            {/* <div className='flex items-center'>
-              <img className='w-[18px] mr-[4px] opacity-40 ' src="/gas.svg" alt="" />
-              <div>$0.23</div>
-              <Arrow color="#cecece" />
-            </div> */}
-          </div>
+          <Tip tag={tag} />
           <div className='w-full rounded-[18px] bg-[#1f7f94] text-[20px] text-white h-[58px] flex justify-center items-center'>
             Swap
           </div>
