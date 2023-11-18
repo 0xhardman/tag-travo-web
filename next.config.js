e2d05681c753bbc63c6b59ba400fc2c7e0fa2d5d
jyanon <804173948@qq.com>
2023-11-18 21:51:26 +0300

    [Enhance] package

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 BE_API = process.env.BE_API
+IMG_API = "demo.data2.cash"
+
 const nextConfig = {
   async rewrites() {
     return [
       {
         source: '/api/:path*',
         destination: `${BE_API}/api/:path*`,
+      },
+      {
+        source: '/img/:path*',
+        destination: `${IMG_API}/img/:path*`,
       }
     ]
   },
